c3e881cf91b6714aee2575a68d6c120a90f98c21
bgabor88 <coindealerpro@gmail.com>
2022-02-15 09:14:07 +0100

    Setup ProductContext for migration

---
+++ src/Codecool.CodecoolShop/ProductContext.cs
@@ -0,0 +1,13 @@
+﻿using Codecool.CodecoolShop.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Codecool.CodecoolShop
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions options) : base(options) {}
+        public DbSet<Product> Product { get; set; }
+        public DbSet<ProductCategory> Category { get; set; }
+        public DbSet<Supplier> Supplier { get; set; }
+    }
+}
