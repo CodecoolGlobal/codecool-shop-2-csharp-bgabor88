56eb0e03b07ec9f628ae2af4ec87efe07b284058
bgabor88 <coindealerpro@gmail.com>
2022-02-15 15:15:24 +0100

    Generate migration script with data

--- src/Codecool.CodecoolShop/Models/ProductCategory.cs
+++ src/Codecool.CodecoolShop/Models/ProductCategory.cs
@@ -1,9 +1,12 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
 
 namespace Codecool.CodecoolShop.Models
 {
     public class ProductCategory: BaseModel
     {
+        [NotMapped]
         public List<Product> Products { get; set; }
         public string Department { get; set; }
     }
