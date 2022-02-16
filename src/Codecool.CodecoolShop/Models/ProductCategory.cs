3ad917506d2ed983a67f99400ff5f18eca3e2bc3
bgabor88 <coindealerpro@gmail.com>
2022-02-16 19:17:23 +0100

    Remove unused properties and using statements

--- src/Codecool.CodecoolShop/Models/ProductCategory.cs
+++ src/Codecool.CodecoolShop/Models/ProductCategory.cs
@@ -1,12 +1,7 @@
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.EntityFrameworkCore;
-
 namespace Codecool.CodecoolShop.Models
 {
     public class ProductCategory: BaseModel
     {
-        public virtual List<Product> Products { get; set; }
         public string Department { get; set; }
     }
 }
--- src/Codecool.CodecoolShop/Models/Supplier.cs
+++ src/Codecool.CodecoolShop/Models/Supplier.cs
@@ -1,12 +1,7 @@
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-
 namespace Codecool.CodecoolShop.Models
 {
     public class Supplier : BaseModel
     {
-        public virtual IEnumerable<Product> Products { get; set; }
-        
         public override string ToString()
         {
             return new string($"Id: {Id} Name: {Name} Description: {Description}");
