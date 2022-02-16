b84b7519555094c759e3da78e67aaa95e09d5627
bgabor88 <coindealerpro@gmail.com>
2022-02-16 19:23:02 +0100

    Remove public property and create a private field instead

--- src/Codecool.CodecoolShop/Controllers/ProductController.cs
+++ src/Codecool.CodecoolShop/Controllers/ProductController.cs
@@ -16,25 +16,25 @@ namespace Codecool.CodecoolShop.Controllers
     public class ProductController : Controller
     {
         private readonly ILogger<ProductController> _logger;
-        public ProductService ProductService { get; set; }
+        private readonly ProductService _productService;
 
         public ProductController(ILogger<ProductController> logger, ProductService service)
         {
             _logger = logger;
-            ProductService = service;
+            _productService = service;
         }
         
         public IActionResult Index()
         {
             SessionHelper.SetCartSession(ViewBag, HttpContext.Session);
-            var products = ProductService.GetAll().OrderBy(x => x.DefaultPrice);
+            var products = _productService.GetAll().OrderBy(x => x.DefaultPrice);
             return View(products.ToList());
         }
 
         [Route("Product/{brand}/{category}")]
         public IActionResult Products(string brand, string category)
         {
-            var products = ProductService.GetFilteredProducts(brand, category).OrderByDescending(x => x.DefaultPrice);
+            var products = _productService.GetFilteredProducts(brand, category).OrderByDescending(x => x.DefaultPrice);
             return PartialView(viewName: "_products", products.ToList());
         }
     }
