0eb94c1af26b415e7a57deef71c992968fb14009
richardmogyoro <richard.mogyoro@gmail.com>
2022-02-17 09:51:26 +0100

    add ProductService tests file

---
+++ src/Codecool.CodecoolShop.UnitTests/ProductServiceTests.cs
@@ -0,0 +1,38 @@
+using NUnit.Framework;
+
+namespace Codecool.CodecoolShop.UnitTests
+{
+    public class ProductServiceTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void GetProductCategory_()
+        {
+            Assert.Pass();
+        }
+
+        public void GetProductsForCategory_()
+        {
+            Assert.Pass();
+        }
+
+        public void GetFilteredProducts_()
+        {
+            Assert.Pass();
+        }
+
+        public void GetAll_()
+        {
+            Assert.Pass();
+        }
+
+        public void GetProductById()
+        {
+            Assert.Pass();
+        }
+    }
+}
